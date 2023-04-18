10b92de3bd9b2a6fcba59b871cd3930d9f03b1f5
nanjingtao <nanjingtao@tal>
2023-04-18 19:46:41 +0800

    dev: 测试iframe地址嵌套

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div>
       <div className="home">React vite首页</div>
+      <iframe className="iframe_box" src="https://www.123pan.com"></iframe>
     </div>
   )
 }
